499a4026886d26fac2f39b6b95a8c3df850e7d3c
Bruno Cabral <cabral.brc@gmail.com>
2024-09-12 19:57:00 -0700

    draw particles

--- Particles.py
+++ Particles.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import sys
 
@@ -7,8 +8,22 @@ pygame.display.set_caption("Particles")
 
 mainClock = pygame.time.Clock()
 
+# [loc, velocity, timer] - timer is also radius
+particles = []
+
 while True:
     screen.fill((0, 0, 0))
+
+    particles.append([[250, 250], [random.randint(0, 20) / 10 - 1, -2], random.randint(4, 6)])
+
+    for particle in particles:
+        # [loc, velocity, timer] - timer is also radius
+        particle[0][0] += particle[1][0]
+        particle[0][1] += particle[1][1]
+        particle[2] -= 0.1  # also radius
+        pygame.draw.circle(screen, (255, 255, 255), particle[0], particle[2])
+        if particle[2] <= 0:
+            particles.remove(particle)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
