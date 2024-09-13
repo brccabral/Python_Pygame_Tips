f9c31c4159b644f1ad46d4f2035f0667ca72e16a
Bruno Cabral <cabral.brc@gmail.com>
2024-09-12 20:23:47 -0700

    created obstacles for particles

--- Particles.py
+++ Particles.py
@@ -2,6 +2,8 @@ import random
 import pygame
 import sys
 
+TILE_SIZE = 16
+
 pygame.init()
 screen = pygame.display.set_mode((500, 500), 0, 32)
 pygame.display.set_caption("Particles")
@@ -18,23 +20,48 @@ class Particle:
 
 particles: list[Particle] = []
 
+tile_map: dict[str, tuple[int, int, tuple[int, int, int]]] = {}
+for i in range(10):
+    tile_map[str(i + 4) + ";14"] = (i + 4, 14, (255, 0, 0))
+
+tile_map["15;10"] = (15, 10, (0, 0, 255))
+tile_map["15;11"] = (15, 11, (0, 0, 255))
+tile_map["15;12"] = (15, 12, (0, 0, 255))
+tile_map["15;13"] = (15, 13, (0, 0, 255))
+
+tile_map["11;11"] = (11, 11, (0, 255, 255))
+tile_map["11;12"] = (11, 12, (0, 255, 255))
+
+clicking = False
+
 while True:
     screen.fill((0, 0, 0))
 
     mouse_pos = pygame.mouse.get_pos()
-    particles.append(Particle([mouse_pos[0], mouse_pos[1]], [random.randint(0, 20) / 10 - 1, -2], random.randint(4, 6)))
+
+    if clicking:
+        particles.append(
+            Particle(
+                [mouse_pos[0], mouse_pos[1]],
+                [random.randint(0, 42) / 6 - 3.5, random.randint(0, 42) / 6 - 3.5],
+                random.randint(4, 6),
+            )
+        )
 
     for particle in particles:
         particle.location[0] += particle.velocity[0]
         particle.location[1] += particle.velocity[1]
-        particle.timer -= 0.1  # also radius
-        pygame.draw.circle(screen, (255, 255, 255), particle.location, particle.timer)
+        particle.timer -= 0.05  # also radius
         particle.velocity[1] += 0.1  # gravity
+        pygame.draw.circle(screen, (255, 255, 255), particle.location, particle.timer)
 
     # remove from a list outside main loop to avoid screen flickering
     for i, p in sorted(enumerate(particles), key=lambda k: k[1].timer, reverse=True):
         if p.timer <= 0:
             particles.pop(i)
+
+    for k, tile in tile_map.items():
+        pygame.draw.rect(screen, tile[2], pygame.Rect(tile[0] * TILE_SIZE, tile[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,6 +71,12 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                clicking = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                clicking = False
 
     pygame.display.update()
     mainClock.tick(60)
