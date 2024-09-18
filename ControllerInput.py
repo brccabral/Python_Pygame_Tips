e6e11fc2237449150d9223ccf2604915c51ee530
Bruno Cabral <cabral.brc@gmail.com>
2024-09-17 18:29:53 -0700

    init controllers

--- ControllerInput.py
+++ ControllerInput.py
@@ -7,6 +7,11 @@ pygame.display.set_caption("game base")
 screen = pygame.display.set_mode((500, 500), 0, 32)
 clock = pygame.time.Clock()
 
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+for joystick in joysticks:
+    print(joystick.get_name())
+
 my_square = pygame.Rect(50, 50, 50, 50)
 my_square_color = 0
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
