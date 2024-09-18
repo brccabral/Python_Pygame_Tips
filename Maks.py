2020fadf85465fd2554f0ec7c9cad5e65125deb4
Bruno Cabral <cabral.brc@gmail.com>
2024-09-17 21:02:03 -0700

    Masks - blit maks

--- Maks.py
+++ Maks.py
@@ -14,19 +14,32 @@ img_2 = pygame.image.load("img_2.png")
 img_2.set_colorkey((0, 0, 0))
 img_loc = (50, 50)
 
+mask = pygame.mask.from_surface(img)
+mask_surf = mask.to_surface()
+mask_2 = pygame.mask.from_surface(img_2)
+mask_2_surf = mask_2.to_surface()
+
+show_masks = False
+
 while True:
     screen.fill((0, 0, 0))
 
     mx, my = pygame.mouse.get_pos()
 
-    screen.blit(img, img_loc)
-    screen.blit(img_2, (mx, my))
+    if not show_masks:
+        screen.blit(img, img_loc)
+        screen.blit(img_2, (mx, my))
+    else:
+        screen.blit(mask_surf, img_loc)
+        screen.blit(mask_2_surf, (mx, my))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_m:
+                show_masks = not show_masks
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
