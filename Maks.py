1d767ce2dfd7d9d585c30b1e1aecb2195738b072
Bruno Cabral <cabral.brc@gmail.com>
2024-09-17 20:58:54 -0700

    Masks - show images

--- Maks.py
+++ Maks.py
@@ -8,10 +8,19 @@ screen = pygame.display.set_mode((500, 500))
 
 clock = pygame.time.Clock()
 
+img = pygame.image.load("img.png")
+img.set_colorkey((0, 0, 0))
+img_2 = pygame.image.load("img_2.png")
+img_2.set_colorkey((0, 0, 0))
+img_loc = (50, 50)
+
 while True:
     screen.fill((0, 0, 0))
 
     mx, my = pygame.mouse.get_pos()
+
+    screen.blit(img, img_loc)
+    screen.blit(img_2, (mx, my))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
