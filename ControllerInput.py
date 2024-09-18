aea1c5bda214e0fe13099b26265f9e91577bd5a1
Bruno Cabral <cabral.brc@gmail.com>
2024-09-17 18:28:37 -0700

    Controller Input init

---
+++ ControllerInput.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+
+
+pygame.init()
+pygame.display.set_caption("game base")
+screen = pygame.display.set_mode((500, 500), 0, 32)
+clock = pygame.time.Clock()
+
+my_square = pygame.Rect(50, 50, 50, 50)
+my_square_color = 0
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+motion = [0, 0]
+
+while True:
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, colors[my_square_color], my_square)
+    my_square.x += motion[0] * 10
+    my_square.y += motion[1] * 10
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
